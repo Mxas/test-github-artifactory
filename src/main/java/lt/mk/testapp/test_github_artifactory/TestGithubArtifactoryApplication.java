d624f3bd90da7e909705c1f08ab35fdf58b26365
Mindaugas <mkarpinskas@exadel.com>
2024-11-11 23:02:40 +0200

    test

---
+++ src/main/java/lt/mk/testapp/test_github_artifactory/TestGithubArtifactoryApplication.java
@@ -0,0 +1,13 @@
+package lt.mk.testapp.test_github_artifactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGithubArtifactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGithubArtifactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/mk/testapp/test_github_artifactory/TestGithubArtifactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.mk.testapp.test_github_artifactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubArtifactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
